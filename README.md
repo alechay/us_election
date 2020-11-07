96308134e2c63d9d2e3565c0b631ad3cb957c7c9
Alec Hay <55260698+alechay@users.noreply.github.com>
2020-11-07 10:49:37 -0500

    county data and dash trial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # us_election
 US presidential election data
+
+## Source
+MIT Election Data and Science Lab, 2017, "U.S. President 1976–2016", https://doi.org/10.7910/DVN/42MVDX, Harvard Dataverse, V5, UNF:6:Mw0hOUHAijKPTVRAe5jJvg== [fileUNF]
+
+MIT Election Data and Science Lab, 2018, "County Presidential Election Returns 2000-2016", https://doi.org/10.7910/DVN/VOQCHQ, Harvard Dataverse, V6, UNF:6:ZZe1xuZ5H2l4NUiSRcRf8Q== [fileUNF]
--- explore.py
+++ explore.py
@@ -1,4 +1,27 @@
 import pandas as pd
+import plotly.graph_objects as go
 
-df=pd.read_csv('dataverse_files/1976-2016-president.csv')
-print(df.head())
+election=pd.read_csv('state/1976-2016-president.csv')
+election['percent']=election['candidatevotes']/election['totalvotes']
+data=election.loc[
+    (election['party']=='democrat')&
+    (election['year']==2000)]
+# print(data)
+
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+# print(df['code'])
+
+fig = go.Figure(data=go.Choropleth(
+    locations=data['state_po'], # Spatial coordinates
+    z = data['percent'], # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Reds',
+    colorbar_title = "Percent",
+))
+
+fig.update_layout(
+    title_text = 'D percent',
+    geo_scope='usa', # limite map scope to USA
+)
+
+fig.show()
