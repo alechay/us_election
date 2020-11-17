d06e5394409c8fc130a62e044d7c3530a6e984b9
Alec Hay <55260698+alechay@users.noreply.github.com>
2020-11-17 16:56:49 -0500

    added requirements.txt

--- explore.py
+++ explore.py
@@ -1,27 +1,19 @@
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
 import pandas as pd
-import plotly.graph_objects as go
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
 
-election=pd.read_csv('state/1976-2016-president.csv')
-election['percent']=election['candidatevotes']/election['totalvotes']
-data=election.loc[
-    (election['party']=='democrat')&
-    (election['year']==2000)]
-# print(data)
+import plotly.express as px
 
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
-# print(df['code'])
-
-fig = go.Figure(data=go.Choropleth(
-    locations=data['state_po'], # Spatial coordinates
-    z = data['percent'], # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Reds',
-    colorbar_title = "Percent",
-))
-
-fig.update_layout(
-    title_text = 'D percent',
-    geo_scope='usa', # limite map scope to USA
-)
-
+fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           scope="usa",
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.show()
